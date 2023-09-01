f05e2caf5ba7ca4f31d7517e17de8338e139f5b7
“Ishansehgal” <sehgalishan26@gmail.com>
2023-09-01 21:10:51 +0530

    j

---
+++ displacement.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import rospy
+from nav_msgs.msg import Path
+from rmp01.srv import posemsg
+
+def plan_callback(msg):
+        global x, y, z 
+        pose_stamped = msg.poses[0]
+        pose = pose_stamped.pose
+        position = pose.position
+
+        # Access the x, y, z coordinates of the position
+        x = position.x
+        y = position.y
+        z = position.z
+        #rospy.loginfo("the values are x={}, y={}, z={}".format(x, y, z))
+        return x,y,z
+
+def handle_get_plan_coordinates(req):
+    global x, y, z 
+
+
+    response = posemsg()
+    response.x = float(x)
+    response.y = float(y)
+    response.z = float(z)
+      
+    rospy.loginfo("the values are x={}, y={}, z={}".format(x, y, z))
+    return response      
+def main():
+    rospy.init_node('plan_subscriber')
+    plan_topic = 'move_base/NavfnROS/plan'
+    rospy.Subscriber(plan_topic, Path, plan_callback)
+    service = rospy.Service('pose_msg', posemsg, handle_get_plan_coordinates)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
